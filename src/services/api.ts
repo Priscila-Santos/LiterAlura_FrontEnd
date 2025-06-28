90e90fb7264a8c18cb354b80b95076a0b4ca15ec
Priscila Santos <priscila15dsantos@gmail.com>
2025-06-28 17:19:11 -0300

    change url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,11 @@
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:8282/api",
+// });
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8282/api",
+  baseURL: "https://literalura-backend.onrender.com/api",
 });
